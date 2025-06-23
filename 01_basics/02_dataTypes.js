78204b63aa2f084b3571250237c21c2bff163408
Tanmay Ahirrao <53315017+tanmayahirrao@users.noreply.github.com>
2025-06-23 17:39:51 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all code as a newer version
+let age = 26
+
+//alert(3 + 3) will not run here. it can run in inspect console. you can run alert here but have different syntax (we are using nodejs and not browser)
+
+// number => 2 to the power 53
+//bigint
+//string => "Tanmay"
+//boolean => true / false
+//null => standalone value
+//undefined => not assigned
+//symbol => unique (if you working on react and creating component to identify the uniqueness of the component we use symbol)
+
+
+//object
+
+console.log(age);
+console.log(typeof age);
+console.log(typeof null) // type object
+console.log(typeof undefined); //type undefined

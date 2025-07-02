383266d9ab78ac98aad26f31833bfdecfc545a51
Tanmay Ahirrao <53315017+tanmayahirrao@users.noreply.github.com>
2025-07-02 19:16:57 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1) //js converted the string 2 to number
+// console.log("02" > 1)
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >=0) // true (because the equality check == and comparisons >, <, >=, <= work differently. comparisons convert null to number i.e 0 thats why is true)
+
+
+console.log('2' == 2) //(True)'2' not a strict check result will be true.
+console.log('2' === 2) // === (False) means strict check this time the number will get check and the datatype will also get check

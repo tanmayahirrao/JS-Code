88a7b3ad7a28ad9601ce9e6f2d7f88ff582fc6d7
Tanmay Ahirrao <53315017+tanmayahirrao@users.noreply.github.com>
2025-12-16 16:44:31 +0000

    asynchronous

---
+++ Asynchronous/async.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+function print(err, data) {
+    console.log(data)
+}
+
+fs.readFile("a.txt", "utf-8", print)
+fs.readFile("b.txt", "utf-8", print)
+
+console.log("Done!")

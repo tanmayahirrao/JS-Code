d94b38b80114b57ffdb712830bea0e56ed57755c
Tanmay Ahirrao <53315017+tanmayahirrao@users.noreply.github.com>
2025-12-18 16:20:59 +0000

    Date class example

---
+++ Asynchronous/DateClass.js
@@ -0,0 +1,5 @@
+const date = new Date()
+console.log(date) //2025-12-18T11:53:25.701Z
+console.log(date.getDay()) //4 (Thursday) returns the day of the week
+console.log(date.getMonth()) // returns the month of the year (0 based indexing)
+console.log(date.getFullYear()) //returns the year in this case 2025
